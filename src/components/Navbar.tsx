620b6bb2b975d20c7a1f3745e96456c5ab1bebe4
Dev Kushwaha <devkushwaha3575@gmail.com>
2024-03-18 23:53:15 +0530

    nav update

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+"use client";
+import reactElementToJSXString from "react-element-to-jsx-string";
+import { ButtonsCard } from "@/components/ui/tailwindcss-buttons";
+import { cn } from "@/utils/cn";
+import Seminav from "./Seminav";
+
+function Navbar() {
+  return (
+    <div className="relative grid grid-cols-12 p-4">
+      <div className="grid col-span-2 relative">
+        <h2 className="text-2xl font-bold ml-4 pl-4 pt-2 relative">
+          QuillAudits
+        </h2>
+      </div>
+      <div className="grid col-span-8 relative">
+        <Seminav />
+      </div>
+      <div className="grid col-span-2 relative">
+        <ButtonsCard className="h-10 w-auto p-4 inset-0 bg-gradient-to-r from-indigo-600 to-indigo-300 rounded-lg">
+          <button className="p-4 mr-10 relative transition duration-1000 text-sm">
+            Request An Audit
+          </button>
+        </ButtonsCard>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
